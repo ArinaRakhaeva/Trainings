c3c1bb042c542bf276081111cf2479edcddca85b
ArinaRakhaeva <raxaevaa@bk.ru>
2019-04-22 21:47:04 +0300

    Task1

--- src/App.js
+++ src/App.js
@@ -1,61 +1,48 @@
 import React, { Component } from 'react';
-import './StyleInput.css'; 
 import './App.css';
-import TodoTable from './components/TodoTable';
-// import ReactTable from 'react-table';
-// import 'react-table/react-table.css';
+import CustomerList from './components/CustomerList';
+import TrainingsList from './components/TrainingsList';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 
 class App extends Component {
- constructor(props){
-   super(props);
-   this.state={description:'', date:'', todos: []}
- }
- inputChanged=(event)=> {
- this.setState({description: event.target.value})
-}
-inputChange=(event)=> {
-  this.setState({date: event.target.value})
- }
- submitForm=(event)=>{
-  event.preventDefault();
-  const newTodo = {
-    description: this.state.description,
-    date: this.state.date
-  };
-  this.setState({
-    description: "",
-    date:"",
-    todos: [...this.state.todos, newTodo]
-  })
-}
- deleteFunction=(row)=>{
-  // const index = parseInt (event.target.id);
-  this.setState({
-    todos: this.state.todos.filter((todo, i) => i !== row),
-  }) 
- }
+
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      currentListName : '',
+      currentList : null
+    }
+
+    if (window.location.pathname === '/customers') {
+      this.state = {
+        currentListName : 'CUSTOMER LIST',
+        currentList : <CustomerList/>
+      }
+    } else if (window.location.pathname === '/trainings') {
+      this.state = {
+        currentListName : 'TRAININGS LIST',
+        currentList : <TrainingsList/>
+      }
+    }
+  }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-        <p>
-          Todo list
-        </p>
-        </header>
-        <div>
-          <form onSubmit={this.submitForm}> 
-          Description:
-          <input type="text" value={this.state.description} onChange={this.inputChanged} />
-          Date:
-          <input type="date" value={this.state.date} onChange={this.inputChange}/>
-        <input type="submit" value="Add"/>
-        </form>
-        </div>
-       <TodoTable todos={this.state.todos} delete={this.deleteFunction}/>     
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" color="inherit">
+              { this.state.currentListName }
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        { this.state.currentList }
       </div>
     );
-    }
   }
+}
 
 export default App;
